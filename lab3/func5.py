71208a6819bb39000b607311ba04603ed459161e
Amina Sabit <sabitamina811@gmail.com>
2025-02-02 21:03:58 +0500

    Update code

--- lab3/func5.py
+++ lab3/func5.py
@@ -1,7 +1,11 @@
-from itertools import permutations
-
-def print_permutations(s):
-    for perm in permutations(s):
-        print(''.join(perm))
+def print_permutations(s, a=""):
+    if len(s) == 0:
+        print(a)
+        return
+    
+    for i in range(len(s)):
+        char = s[i]
+        remaining = s[:i] + s[i+1:]
+        print_permutations(remaining, a + char)
 
 print_permutations(input("Enter: "))
